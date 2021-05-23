4f97200499d959f5a67fb4492b5de1f5d1700ff3
Scott Geleas <scottgeleas97@gmail.com>
2021-05-23 09:53:08 -0400

    file structure

---
+++ db/company_db.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    `id` INT NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    'id' INT NOT NULL,
+    `title` VARCHAR(30) NOT NULL,
+    `salary` DECIMAL NOT NULL,
+    `department_id` INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    'id' INT NOT NULL,
+    `first_name` VARCHAR(30) NOT NULL,
+    `last_name` VARCHAR(30) NOT NULL,
+    `role_id` INT NOT NULL,
+    `manger_id` INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO department (`id`, `name`)
+VALUES (101, "Marketing");
+
+
+
